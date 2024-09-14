3fe5cf7fbabe512b64864187c24ac573193911c4
mb8504 <mbergmarketing@gmail.com>
2024-09-14 08:05:26 -0400

    updated images to webp

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
                 <div className="flex justify-center">
                     <div className="flex flex-col md:flex-row justify-center mt-20">
                         <div className="w-72">
-                            <img src="/SiteImages/locals.png" alt="" />
+                            <img src="/SiteImages/locals.webp" alt="" />
                         </div>
                         <div className="bg-white w-72 md:w-96">
                             <h2 className="text-siteOrange font-bold text-center mt-4 mb-2 text-2xl">Local Buzz Brewing</h2>
@@ -19,7 +19,7 @@ const Projects = () => {
                                 A custom designed local brewery website built with Vite, React, TailwindCSS, and GSAP. 
                             </p>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4" href="https://local-buzz-brewing.netlify.app/" target="_blank">
-                                <img className="w-10 pt-1 pb-1" src="/SiteImages/web_icon.png" alt=""/>
+                                <img className="w-10 pt-1 pb-1" src="/SiteImages/Untitled design (36).webp" alt=""/>
                                 <p className="text-[#7691ff] ml-3">View Site</p>
                             </a>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4 mb-4 md:mb-0" href="https://github.com/mb8504/local_buzz_brewing" target="_blank">
@@ -33,7 +33,7 @@ const Projects = () => {
                 <div className="flex justify-center">
                     <div className="flex flex-col md:flex-row justify-center mt-20">
                         <div className="w-72">
-                            <img src="/SiteImages/coffee_card.png" alt="" />
+                            <img src="/SiteImages/coffee_card.webp" alt="" />
                         </div>
                         <div className="bg-white w-72 md:w-96">
                             <h2 className="text-siteOrange font-bold text-center mt-4 mb-2 text-2xl">True Believers Coffee</h2>
@@ -41,7 +41,7 @@ const Projects = () => {
                                 A local coffee shop site. The site was built with HTML, CSS, and vanilla JavaScript.
                             </p>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4" href="https://mb8504.github.io/tb_coffee/" target="_blank">
-                                <img className="w-10 pt-1 pb-1" src="/SiteImages/web_icon.png" alt=""/>
+                                <img className="w-10 pt-1 pb-1" src="/SiteImages/Untitled design (36).webp" alt=""/>
                                 <p className="text-[#7691ff] ml-3">View Site</p>
                             </a>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4 mb-4 md:mb-0" href="https://github.com/mb8504/tb_coffee" target="_blank">
@@ -55,7 +55,7 @@ const Projects = () => {
                 <div className="flex justify-center">
                     <div className="flex flex-col md:flex-row justify-center mt-20">
                         <div className="w-72">
-                            <img src="/SiteImages/ms_hero1.png" alt="" />
+                            <img src="/SiteImages/ms_hero1 (1).webp" alt="" />
                         </div>
                         <div className="bg-white w-72 md:w-96">
                             <h2 className="text-siteOrange font-bold text-center mt-4 mb-2 text-2xl">The Marketing Spot</h2>
@@ -63,7 +63,7 @@ const Projects = () => {
                                 A digital marketing agency custom built with HTML, CSS, and vanilla JavaScript. 
                             </p>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4" href="https://mb8504.github.io/digital_marketing_site/" target="_blank">
-                                <img className="w-10 pt-1 pb-1" src="/SiteImages/web_icon.png" alt=""/>
+                                <img className="w-10 pt-1 pb-1" src="/SiteImages/Untitled design (36).webp" alt=""/>
                                 <p className="text-[#7691ff] ml-3">View Site</p>
                             </a>
                             <a className="flex flex-row justify-center items-center border-2 border-[#7691ff] rounded ml-8 mr-8 mt-4 mb-4 md:mb-0" href="https://github.com/mb8504/digital_marketing_site" target="_blank">

803816195b59679b7e57fc0fa1688903c39d4bb2
mb8504 <mbergmarketing@gmail.com>
2024-09-14 09:02:56 -0400

    updated w/ react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import Home from "./pages/Home/Home"
-import About from "./pages/About/About"
-import Projects from "./pages/Projects/Projects"
+// import Home from "./pages/Home/Home"
+// import About from "./pages/About/About"
+// import Projects from "./pages/Projects/Projects"
+// import Technologies from "./pages/Technologies/Technologies"
 import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from "react-router-dom"
-import Technologies from "./pages/Technologies/Technologies";
+import { lazy } from "react"
+
+const Home = lazy(() => import("./pages/Home/Home"))
+const About = lazy(() => import("./pages/About/About"))
+const Projects = lazy(() => import("./pages/Projects/Projects"))
+const Technologies = lazy(() => import("./pages/Technologies/Technologies"))
+
 
 
 const router = createBrowserRouter(
